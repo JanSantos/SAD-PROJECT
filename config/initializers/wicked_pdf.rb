d7005ab650884581ae064673854ebf76f5ae8802
JanSantos <jan.santos04@gmail.com>
2015-06-02 15:36:58 +0800

    pdf heroku2

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,13 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => "/usr/local/bin/wkhtmltopdf"
-}
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf"
+else
+  # Linux (check your processor for Intel x86 or AMD x64)
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf_linux_x86"
+  # OS X
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf_darwin_x86"            
+  # Windows
+  # wkhtmltopdf_path = 'C:\Program Files/wkhtmltopdf/wkhtmltopdf.exe'
+end
+
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
